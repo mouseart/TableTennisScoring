51be6828036b89c3eb5cbccbe75181407ab5cde7
mouseart <fengl@funland.cn>
2017-11-25 00:40:05 +0800

    update & add 增加了开始下一局的Activity,修改了ScoringActivity,现在本局结束的时候会跳转到开始下一局。

---
+++ app/src/main/java/mouseart/com/github/tabletennisscoring/NextGameActivity.java
@@ -0,0 +1,20 @@
+package mouseart.com.github.tabletennisscoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * NextGameActivity class
+ * 开始下一局
+ * @author Leon Feng
+ * @date 2017/11/16
+ */
+public class NextGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_game);
+    }
+}
--- app/src/main/java/mouseart/com/github/tabletennisscoring/ScoringActivity.java
+++ app/src/main/java/mouseart/com/github/tabletennisscoring/ScoringActivity.java
@@ -1,6 +1,7 @@
 package mouseart.com.github.tabletennisscoring;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -28,6 +29,10 @@ import static mouseart.com.github.tabletennisscoring.StartNewGameActivity.scoreL
  * @date 2017/11/16
  */
 public class ScoringActivity extends AppCompatActivity {
+
+    private static final int KEEP_ON_PLAYING = 0;
+    private static final int TEAM_A_WIN = 1;
+    private static final int TEAM_B_WIN = 2;
 
     //TeamA的得分
     public int scoreTeamA = 0;
@@ -166,16 +171,17 @@ public class ScoringActivity extends AppCompatActivity {
         displayDatabaseInfo();*/
 
         switch (tableTennisScoreJudgment(scoreTeamA, scoreTeamB)) {
-            case 0:
+            case KEEP_ON_PLAYING:
                 System.out.println("A+1比赛继续");
                 displayForTeamA(scoreTeamA);
                 rounds++;
                 displayForTeamStatus(scoreTeamA, scoreTeamB, rounds);
                 break;
-            case 1:
+            case TEAM_A_WIN:
                 System.out.println("A获得本回合胜利！");
                 displayForTeamA(scoreTeamA);
                 rounds++;
+                scoreAllA++;
                 displayForTeamViewA("TeamAName 获得本回合胜利！");
                 displayForTeamViewB("TeamBName ");
 
@@ -183,6 +189,11 @@ public class ScoringActivity extends AppCompatActivity {
                 /*eventType=411;
                 mGameLogDao.insertData(gameId,eventType,gameScore);
                 displayDatabaseInfo();*/
+
+                //跳转到下一个Activity
+                Intent intent = new Intent(ScoringActivity.this, NextGameActivity.class);
+                startActivity(intent);
+
 
             default:
                 System.out.println("返回结果错误！");
@@ -202,22 +213,28 @@ public class ScoringActivity extends AppCompatActivity {
         mGameLogDao.insertData(gameId,eventType,gameScore);*/
         // displayDatabaseInfo();
         switch (tableTennisScoreJudgment(scoreTeamA, scoreTeamB)) {
-            case 0:
+            case KEEP_ON_PLAYING:
                 System.out.println("B+1比赛继续");
                 displayForTeamB(scoreTeamB);
                 rounds++;
                 displayForTeamStatus(scoreTeamA, scoreTeamB, rounds);
                 break;
-            case 2:
+            case TEAM_B_WIN:
                 System.out.println("B获得本回合胜利！");
                 displayForTeamB(scoreTeamB);
                 rounds++;
+                scoreAllB++;
                 displayForTeamViewA("TeamAName ");
                 displayForTeamViewB("TeamBName 获得本回合胜利！");
+
                 //写入TeamB获得本回合胜利日志
                 /*eventType=421;
                 mGameLogDao.insertData(gameId,eventType,gameScore);
                 displayDatabaseInfo();*/
+
+                //跳转到下一个Activity
+                Intent intent = new Intent(ScoringActivity.this, NextGameActivity.class);
+                startActivity(intent);
 
             default:
                 System.out.println("返回结果错误！");
@@ -227,25 +244,25 @@ public class ScoringActivity extends AppCompatActivity {
 
 
     /**
-     * 排球比赛得分胜负判定
+     * 比赛得分胜负判定
      * 前2个参数为2个选手的得分；
-     * 第3个参数，通常为11，也可传入21修改为21球的规则（或任意球的规则）；
+     * 如需要，可设置第3个参数，通常为11，也可传入21修改为21球的规则（或任意球的规则）；
      * 返回参数：0-继续比赛；1-A本局获胜；2-B本局获胜；
      */
     private static int tableTennisScoreJudgment(int scoreA, int scoreB) {
         int result;
         if ((scoreA <= scoreLimit - 1) && (scoreB <= scoreLimit - 1)) {
-            result = 0;
+            result = KEEP_ON_PLAYING;
         } else if ((scoreA == scoreLimit) && (scoreB < scoreLimit - 1)) {
-            result = 1;
+            result = TEAM_A_WIN;
         } else if ((scoreA < scoreLimit - 1) && (scoreB == scoreLimit)) {
-            result = 2;
+            result = TEAM_B_WIN;
         } else if ((scoreA >= scoreLimit - 1) && (scoreB >= scoreLimit - 1) && (Math.abs(scoreA - scoreB) < 2)) {
-            result = 0;
+            result = KEEP_ON_PLAYING;
         } else if (scoreA > scoreB) {
-            result = 1;
+            result = TEAM_A_WIN;
         } else {
-            result = 2;
+            result = TEAM_B_WIN;
         }
         return result;
     }
